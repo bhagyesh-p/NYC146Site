b20c8e9cbb1777777a4ca3745962e7debf3d1420
Ryan Qin <rqin@stevens.edu>
2020-07-15 16:31:21 -0400

    Implemented more functionality

--- data/users.js
+++ data/users.js
@@ -2,7 +2,7 @@ const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
 const { ObjectId } = require('mongodb');
 const bcrypt = require('bcrypt');
-const saltRounds = 5;
+const saltRounds = 8;
 
 module.exports = {
     async getUser(id) {
@@ -41,54 +41,39 @@ module.exports = {
         const all = await userCollection.find({}).toArray();
         return all;
     },
-    // async removeBand(id) {
-    //     if (!id) throw 'You must provide an id to search for.';
-    //     const bandCollection = await bands();
-    //     // const albumCollection = await albums();
-    //     if (typeof id == 'string') {
-    //         id = ObjectId.createFromHexString(id);
-    //     }
-    //     // const band = await this.getBand(id);
-    //     // const albums = band.albums;
-    //     // console.log(albums);
-    //     // for (let album of albums) {
-    //     //     console.log(album);
-    //     //     const albumDelete = await albumCollection.deleteOne({ _id: album._id });
-    //     //     if (albumDelete.deletedCount === 0) throw `Could not delete album with id of ${album}`;
-    //     // }
-	// 	const deletionInfo = await bandCollection.deleteOne({ _id: id });
-	// 	if (deletionInfo.deletedCount === 0) throw `Could not delete band with id of ${id}`;
-	// 	return true;
-	// },
-    // async updateBand(bandId, bandName, bandMembers, yearFormed, genres, recordLabel) {
-    //     if (!bandId) throw 'You must provide an id to search for.';
-    //     if (!bandName) throw 'You must provide a band name.';
-    //     if (!Array.isArray(bandMembers)) throw 'You must provide an array of band members.';
-    //     if (!yearFormed) throw 'You must provide the year the band formed.';
-    //     if (!Array.isArray(genres)) throw 'Genres needs to be an array.';
-    //     if (!recordLabel) throw 'You must provide the record label.';
-    //     if (bandMembers.length <= 0) throw 'You must provide band members.';
-    //     if (genres.length <= 0) throw 'You must provide genres.';
+    async removeUser(id) {
+        if (!id) throw 'You must provide an id to search for.';
+        const userCollection = await users();
+        if (typeof id == 'string') {
+            id = ObjectId.createFromHexString(id);
+        }
+		const deletionInfo = await userCollection.deleteOne({ _id: id });
+		if (deletionInfo.deletedCount === 0) throw `Could not delete user with id of ${id}`;
+		return true;
+	},
+    async updateUser(userId, name, email, password) {
+        if (!userId) throw 'You must provide an id to search for.';
+        if (!name) throw 'You must provide a name.';
+        if (!email) throw 'You must provide an email.';
+        if (!password) throw 'You must provide a password.';
 
-    //     if (typeof bandId == 'string') {
-    //         bandId = ObjectId.createFromHexString(bandId);
-    //     }
-    //     const bandCollection = await bands();
-    //     const band = await this.getBand(bandId);
-    //     const updatedBand = {
-    //         bandName: bandName,
-    //         bandMembers: bandMembers,
-    //         yearFormed: yearFormed,
-    //         genres: genres,
-    //         albums: band.albums,
-    //         recordLabel: recordLabel
-    //     };
+        if (typeof userId == 'string') {
+            bandId = ObjectId.createFromHexString(bandId);
+        }
+        const userCollection = await users();
+        const user = await this.getUser(userId);
+        const hash = await bcrypt.hash(password, saltRounds);
+        const updatedUser = {
+            name: name,
+            email: email,
+            password: hash
+        };
 
-    //     const updatedInfo = await bandCollection.updateOne({_id: bandId}, {$set: updatedBand});
-    //     if (updatedInfo.modifiedCount === 0) {
-    //         throw 'Could not update band successfully.';
-    //     }
+        const updatedInfo = await userCollection.updateOne({_id: userId}, {$set: updatedUser});
+        if (updatedInfo.modifiedCount === 0) {
+            throw 'Could not update user successfully.';
+        }
 
-    //     return await this.getBand(bandId);
-    // }
+        return await this.getUser(userId);
+    }
 }
